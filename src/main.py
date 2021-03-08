2e5d9a406ddacc76fc9534ec56af5dcb30320b77
Domehoff <domhof@live.com>
2021-03-07 19:36:16 -0500

    Updates
    main.py:
     added routing to the results page from the search bar
    
    templates
     implemented these search bar fixes in templates: index.html, aboutus.html, results.html,

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
 def hash_password(password, salt):
-    """This will give us a hashed password that will be extremlely difficult to 
+    """This will give us a hashed password that will be extremlely difficult to
     reverse.  Creating this as a separate function allows us to perform this
     operation consistently every time we use it."""
     encoded = password.encode("utf-8")
@@ -39,9 +39,14 @@ def home():
     # add user variable to homepage
     return render_template("index.html", user = user)
 
+@app.route("/", methods=["POST"])
+def search():
+    query = request.form.get("query")
+    return render_template("results.html")
+
 @app.route("/register", methods=["GET"])
 def serve_register_form():
-    
+
     return render_template("register.html")
 
 @app.route("/register", methods=["POST"])
@@ -51,11 +56,11 @@ def register():
     password = request.form.get("password")
 
     # TODO: check if user exist in database
-    
+
     # generate password hash to store
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode("utf-8")
     password_hash = hash_password(password, salt)
-    
+
     # store user in db
     user_key = ds_client.key("UserCredential", username)
     user = datastore.Entity(key=user_key)
@@ -85,15 +90,15 @@ def handle_login():
     # if it got here, that username is in our db
     pw_salt = user["salt"]
     pw_hash = hash_password(password, pw_salt)
-    
+
     # check if hashes match
     if pw_hash != user["password_hash"]:
         return "Password invalid"
-    
+
     # make new session and redirect to home page
     session["user"] = username
     return redirect("/")
-	
+
 @app.route("/aboutus", methods=["GET"])
 def about_us():
 	return render_template("aboutus.html")
@@ -103,8 +108,13 @@ def profile():
 
 	#load the user from the session. You can only access the profile page after logging in, so this should always load.
 	user = session.get("user", None)
-	
+
 	return render_template("profile.html", user = user)
-	
+
+@app.route("/results", methods=["GET"])
+def render_results():
+    text = request.form.get("squery")
+    return render_template("results.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True) 
+    app.run(host='0.0.0.0', port=8080, debug=True)
