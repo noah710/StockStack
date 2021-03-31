c9195d2ab0e0fa5ef5b0cfd52b10797bd8c78c7f
Domehoff <domhof@live.com>
2021-03-31 16:27:13 -0400

    added DOM jquery to fetch yahoo finance data (unimplemented)

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 
+
+
+
+
+$(document).ready(function() {
+// all yahoo finance data will be queried here once the page is loaded.
+
+});
 // testing search results
 function searchResults(query) {
   return 'query';
